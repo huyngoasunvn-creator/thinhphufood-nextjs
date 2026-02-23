03398e005a7832b43815d1a326a04ab4ffcdef09
Huy Ngo <huyngo@gmail.com>
2026-02-23 21:47:25 +0700

    fix news page build

--- app/tin-tuc/page.tsx
+++ app/tin-tuc/page.tsx
@@ -4,5 +4,7 @@ import NewsPage from '@/components/news/News';
 export default async function TinTucPage() {
   const news = await getNewsServer();
 
+  console.log("NEWS DATA:", news);
+
   return <NewsPage news={news} />;
 }
