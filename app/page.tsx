ad342bde9a7c57274c636f936a660e19895b7140
Huy Ngo <huyngo@gmail.com>
2026-02-24 14:14:43 +0700

    test slug route

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import HomePage from "@/components/home/HomePage";
 import { getHomeData } from "@/lib/home-server";
 
+export default function Home() {
+  return <div>TEST 123</div>;
+}
 export default async function Page() {
   const data = await getHomeData();
 
