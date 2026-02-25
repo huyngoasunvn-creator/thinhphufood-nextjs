14281f06232d110d76ae69b359f100c0121be948
Huy Ngo <huyngo@gmail.com>
2026-02-25 23:00:57 +0700

    hhh

--- app/admin/banners/page.tsx
+++ app/admin/banners/page.tsx
@@ -1,13 +1,7 @@
+"use client";
 
-'use client';
+import Banners from "./Banners";
 
-import React from 'react';
-import AdminBanners from '@/components/admin/Banners';
-import { useAppState } from '../../../hooks/useAppState';
-export const dynamic = "force-dynamic";
-
-export default function AdminBannersPage() {
-  const { banners, saveBanners } = useAppState();
-
-  return <AdminBanners banners={banners} onUpdate={saveBanners} />;
+export default function Page() {
+  return <Banners />;
 }
