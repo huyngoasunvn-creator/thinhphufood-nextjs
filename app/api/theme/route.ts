4591a19f4a8b9687fbec3dedd4e4e07106168a72
Huy Ngo <huyngo@gmail.com>
2026-03-04 19:07:18 +0700

    fgfd

--- app/api/theme/route.ts
+++ app/api/theme/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import { adminDb } from "@/lib/firebase-admin"
-
+export const dynamic = "force-dynamic"
 export const runtime = "nodejs" // 🔥 THÊM DÒNG NÀY
 
 export async function GET() {
